d8cd31bae8d524c35d4bc536602a567926afda73
akinoriosamura <osamura.akinori@gmail.com>
2017-11-21 19:03:37 +0900

    a

--- db/migrate/20171118052605_add_role_to_admin_users.rb
+++ db/migrate/20171118052605_add_role_to_admin_users.rb
@@ -1,6 +1,6 @@
 class AddRoleToAdminUsers < ActiveRecord::Migration[5.1]
   def change
     # member: 0, questioner: 1, professional: 2, admin: 10
-    add_column :users, :role, :integer, null: false, default: 0
+    add_column :users, :role, :integer, null: false, default: 10
   end
 end
