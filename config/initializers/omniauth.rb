d6b4d79517ac20dff23fa7775f682fc33d60a5bf
akinoriosamura <osamura.akinori@gmail.com>
2017-12-22 19:37:40 +0900

    update key for getting from env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys!
-   provider :facebook, OAUTH_CONFIG[:facebook]['key'], OAUTH_CONFIG[:facebook]['secret'], scope: ''
-   provider :stripe_connect,   OAUTH_CONFIG[:stripe_connect]['client_id'], OAUTH_CONFIG[:stripe_connect]['secret']
+   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: ''
+   provider :stripe_connect, ENV['STRIPE_CLIENT_ID'], ENV['STRIPE_SECRET']
+   byebug
 end
