df9da4a6060360634da12040e0e122edad1acdbd
akinoriosamura <osamura.akinori@gmail.com>
2017-12-27 21:54:16 +0900

    change env name of heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,13 @@
 CarrierWave.configure do |config|
-  config.asset_host = 'https://qaappserver.s3.amazonaws.com'
-
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['aws_access_key_id'],
-    aws_secret_access_key: ENV['aws_secret_access_key'],
-    region: ENV['region']
+    aws_access_key_id: ENV['S3_ACCESS_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    region: ENV['S3_REGION']
   }
 
   # S3のパケット名
-  config.fog_directory  = 'qaappserver'
+  config.fog_directory  = ENV['S3_BUCKET']
   config.cache_storage = :fog
 
 end
