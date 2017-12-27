02ccf8579e04a287fa5b96c3fef61a39e3dee346
akinoriosamura <osamura.akinori@gmail.com>
2017-12-27 21:42:47 +0900

    add header of aws image url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,6 @@
 CarrierWave.configure do |config|
+  config.asset_host = 'https://qaappserver.s3.amazonaws.com'
+
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['aws_access_key_id'],
