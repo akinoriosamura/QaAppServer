eadb9bfe4783d9af82fffa7a16c9442be43ec006
akinoriosamura <osamura.akinori@gmail.com>
2017-12-28 19:22:35 +0900

    add if else to storage of image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,28 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  # process resize_to_limit: [400, 400]
 
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
-    %w(png jpg)
+    %w(png jpg jpeg PNG)
   end
 
+  # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
+  # 日付(20131001.jpgみたいなファイル名)で保存する
+  """
   def filename
-    original_filename if original_filename
+    time = Time.now
+    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+    name.downcase
   end
+  """
 end
